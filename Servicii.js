beeb776b95a39cb1b07402e7aeb3eca5d23dfa4b
IoanaMoldoveanu <105373419+IoanaMoldoveanu@users.noreply.github.com>
2022-05-14 14:57:25 +0300

    Modificari la servicii si ss, css la preturi, despre noi si contact

---
+++ Servicii.js
@@ -0,0 +1,42 @@
+function myFunction() {
+    document1.open("text/html","replace");
+    document1.write("<h2>Toate supermarketurile din Romania:</h2>");
+    document1.close();
+  }
+  body document1 {
+    background-image: url("emblema.jfif");
+    background-repeat: repeat-x;
+  }
+body {
+    background-color: lightblue;
+  }
+  function myFunction1() {
+    document.open("text/html","replace");
+    document.write("<h2> about the HTML DOM is fun!</h2>");
+    document.close();
+  }
+  function myFunction2() {
+    document.open("text/html","replace");
+    document.write("<h2> about the HTML DOM is fun!</h2>");
+    document.close();
+  }
+  function myFunction3() {
+    document.open("text/html","replace");
+    document.write("<h2> about the HTML DOM is fun!</h2>");
+    document.close();
+  }
+  function myFunction4() {
+    document.open("text/html","replace");
+    document.write("<h2> about the HTML DOM is fun!</h2>");
+    document.close();
+  }
+  function myFunction5() {
+    document.open("text/html","replace");
+    document.write("<h2> about the HTML DOM is fun!</h2>");
+    document.close();
+  }
+  function myFunction6() {
+    document.open("text/html","replace");
+    document.write("<h2> about the HTML DOM is fun!</h2>");
+    document.close();
+  }
