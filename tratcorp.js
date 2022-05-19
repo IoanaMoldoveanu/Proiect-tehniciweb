d0956f82dce4765ca9d209eacf86079d038cf43f
IoanaMoldoveanu <105373419+IoanaMoldoveanu@users.noreply.github.com>
2022-05-19 12:04:21 +0300

    Adaugare setTimeout()

---
+++ tratcorp.js
@@ -0,0 +1,7 @@
+const myTimeout = setTimeout(myGreeting, 2000);
+
+function myGreeting() {
+  document.getElementById("demo1").innerHTML = "V-shape"
+  document.getElementById("demo2").innerHTML ="Ava shape" 
+  document.getElementById("demo3").innerHTML ="Eximia"
+}
